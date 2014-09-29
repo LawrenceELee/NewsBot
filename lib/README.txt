fef9e8818ed38f6e753b5701abb8ed23bfb54e7d
Lawrence L <none@example.com>
2014-09-28 19:08:30 -0700

    Converted to ant build.

---
+++ lib/README.txt
@@ -0,0 +1,1 @@
+Put 3rd party libraries that you are using here.
---
+++ src/NewsBot.java
@@ -0,0 +1,11 @@
+/** *************************************************
+ * Twitter bot for news, current events, etc.
+ *
+ * ************************************************** */
+
+class NewsBot{
+
+    public static void main(String args[]){
+        
+    }
+}

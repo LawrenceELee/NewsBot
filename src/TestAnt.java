43a5dfcf06bdedb80aeed2f5042905c8564a253b
Lawrence L <none@example.com>
2014-09-30 14:00:49 -0700

    Added a dummy program to make sure things are building and running correctly, to maintain your sanity.

---
+++ src/TestAnt.java
@@ -0,0 +1,9 @@
+/** *************************************************
+ * Example java file to test if Ant is building and running tasks correctly.
+ * ************************************************** */
+
+public class TestAnt{
+    public static void main(String args[]){
+        System.out.println("Ant task running!!!");
+    }
+}
